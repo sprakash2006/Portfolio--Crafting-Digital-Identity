ebc43c84b88119a187c6b7b16a8765be90f16b36
praka <prakashswami150569@gmail.com>
2025-09-30 22:37:51 +0530

    minor updated

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -7,7 +7,7 @@ import MovingStrip from '../components/movingStripe'
 const Skills = () => {
   return (
     <div className='skill-page border-t-2 border-[#ff0000]'>
-      <p className='skills-title'>Tech&nbsp;&nbsp;&nbsp;Stack&nbsp;</p>
+      <p className='skills-title'>Tech&nbsp;Stack&nbsp;</p>
       <div className='skill-content'>
         <ProgrammingLanguagesCard />
       </div>
