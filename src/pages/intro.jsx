5e0248ffd0cd7affbc3f6278c381d029a912d661
praka <prakashswami150569@gmail.com>
2025-10-12 14:28:15 +0530

    updated intro

--- src/pages/intro.jsx
+++ src/pages/intro.jsx
@@ -4,15 +4,14 @@ import linkedin from "../assets/linkedin-logo.png";
 import github from "../assets/github-logo.png";
 import leetcode from "../assets/leetcode-logo.png";
 import cv from "../assets/cloud-download.png";
-import RotatingCuboid from "../components/RotatingCuboid";
 import cvPDF from '../assets/CVPrakashSwami.pdf';
-import myphoto from '../assets/profP.jpg'
+import myphoto from '../assets/myimg1.jpg'
 import useMediaQuery from '../Hooks/useMediaQuery';
 
 
 const Intro = () => {
 
-  const isMobile = useMediaQuery("(max-width: 391px)");
+  const isMobile = useMediaQuery("(max-width: 450px)");
 
   return (
     <div className='h-[46vw] w-[100vw] relative main-home-content'>
@@ -23,13 +22,13 @@ const Intro = () => {
           <p>"Versatile creator of innovative, efficient, and user-focused digital solutions across web and beyond."</p>
         </div>
         <div className='sociallogo-div flex gap-5'>
-          <a href="https://www.linkedin.com/in/prakash-swami32431" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/prakash-swami/" target="_blank" rel="noopener noreferrer">
             <img className='socialLogo' src={linkedin} alt="linkedin-logo" />
           </a>
-          <a href="https://leetcode.com/u/sprakash" target="_blank" rel="noopener noreferrer">
+          <a href="https://leetcode.com/u/sprakash_001/" target="_blank" rel="noopener noreferrer">
             <img className='socialLogo' src={leetcode} alt="leetcode-logo" />
           </a>
-          <a href="https://github.com/sprakash" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/sprakash2006" target="_blank" rel="noopener noreferrer">
             <img className='socialLogo' src={github} alt="github-logo" />
           </a>
           <a href={cvPDF} target="_blank" rel="noopener noreferrer">
